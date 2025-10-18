d851903dc39ce66723a0283a0e83e4d1e079c814
Kunj Desai <kunjd2803@gmail.com>
2025-10-18 11:51:30 +0530

    Uncomment NLTK data download lines

--- utils.py
+++ utils.py
@@ -11,12 +11,12 @@ import nltk
 import nltk
 nltk.data.path.append(r'nltk_data')
 
-# # Download required NLTK data
-# nltk.download('punkt')
-# nltk.download('punkt_tab')
-# nltk.download('averaged_perceptron_tagger')
-# nltk.download('wordnet')
-# nltk.download('omw-1.4')
+# Download required NLTK data
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 
 # Initialize lemmatizer
 lemmatizer = WordNetLemmatizer()
