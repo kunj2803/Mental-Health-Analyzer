d5099a1d56c36adef92c3d813d7a1d29be16b89c
Kunj Desai <kunjd2803@gmail.com>
2025-10-18 11:20:58 +0530

    Add download for punkt_tab NLTK data

--- utils.py
+++ utils.py
@@ -10,6 +10,7 @@ import nltk
 
 # Download required NLTK data
 nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
